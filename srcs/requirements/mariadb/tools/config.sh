6e75857f7ce73978e123e42371324e20c2ff82e8
Abderrahmane Belqasmi <abelqasm@e2r12p1.1337.ma>
2023-04-08 16:33:01 +0000

    all setups to the first wordpress instalation

--- srcs/requirements/mariadb/tools/config.sh
+++ srcs/requirements/mariadb/tools/config.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
 service mysql start
-mysql -u root -e "CREATE DATABASE wordpress;"
-mysql -u root -e "CREATE USER 'vendetta'@'localhost' IDENTIFIED BY 'vendetta123';"
-mysql -u root -e "GRANT ALL PRIVILEGES ON wordpress.* TO 'vendetta'@'localhost';"
+mysql -u root -e "CREATE DATABASE $DB_NAME;"
+mysql -u root -e "CREATE USER '$DB_USER'@'%' IDENTIFIED BY '$DB_USER_PW';"
+mysql -u root -e "GRANT ALL PRIVILEGES ON $DB_NAME.* TO '$DB_USER'@'%';"
 mysql -u root -e "FLUSH PRIVILEGES;"
-
+# mysql -u root -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '$DB_ROOT_PW';"
+# mysql -u root -e -p "FLUSH PRIVILEGES;"
--- srcs/requirements/wordpress/tools/config.sh
+++ srcs/requirements/wordpress/tools/config.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
 
-# service php7.3-fpm start
+echo "listen = 9000" >> /etc/php/7.3/fpm/pool.d/www.conf
+
+mkdir /run/php
+chown www-data:www-data /run/php
+wp config create --dbname=$DB_NAME --dbuser=$DB_USER --dbpass=$DB_USER_PW --dbhost=$DB_HOST --dbprefix=wp_ --path=/var/www/html --allow-root
+wp core install --url=$URL --title=$TITLE --admin_user=$DB_USER --admin_password=$DB_USER_PW --admin_email=$USER_EMAIL --path=/var/www/html --allow-root
 php-fpm7.3 -F
