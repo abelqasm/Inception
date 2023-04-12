079fca4e2a8911e4ccdc29a13b511ef628df4ca9
vendetta <abderrahmane.belqasmi@gmail.com>
2023-04-12 18:40:04 +0000

    final steps to finish

--- Makefile
+++ Makefile
@@ -1,2 +1,8 @@
 up:
 	cd ./srcs && docker-compose up
+dowm:
+	cd ./srcs && docker-compose down
+build:
+	cd ./srcs && docker-compose build
+clear:
+	cd ./srcs && docker-compose down --rmi all --volumes 
--- srcs/requirements/wordpress/tools/config.sh
+++ srcs/requirements/wordpress/tools/config.sh
@@ -5,5 +5,6 @@ echo "listen = 9000" >> /etc/php/7.3/fpm/pool.d/www.conf
 mkdir /run/php
 chown www-data:www-data /run/php
 wp config create --dbname=$DB_NAME --dbuser=$DB_USER --dbpass=$DB_USER_PW --dbhost=$DB_HOST --dbprefix=wp_ --path=/var/www/html --allow-root
-wp core install --url=$URL --title=$TITLE --admin_user=$DB_USER --admin_password=$DB_USER_PW --admin_email=$USER_EMAIL --path=/var/www/html --allow-root
+wp core install --url=$URL --title=$TITLE --admin_user=$DB_USER --admin_password=$DB_USER_PW --admin_email=vendetta@docker.com --path=/var/www/html --allow-root
+wp user create $USER_NAME abelqasm@docker.com --role=author --user_pass=$USER_PW --path=/var/www/html --allow-root
 php-fpm7.3 -F
